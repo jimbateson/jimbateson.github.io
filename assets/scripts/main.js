44ca65a334375a73a3570777c5ad24bd7791c208
James Bateson <jim.bateson@outlook.com>
2018-07-25 20:50:48 +0100

    Horizontal scroll issue, and hours based message
    Fixed a weird horizontal scroll issue, I think due to the blockquote. Also added a slightly different message if the time 5pm or after as the market will be over, so he was here!

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,6 +5,7 @@ const day = date.getDate();
 const dayOfWeek = date.getDay() + 1;
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
+const hours = date.getHours();
 const currentDate = `${day}/${month}/${year}`;
 const dateElement = document.querySelector('.js-current-date');
 const messageElement = document.querySelector('.js-is-he-here');
@@ -20,19 +21,27 @@ fetch('./config.json')
 
 	// If it's Wednesday or Saturday and he's here, show yes
 
-	if((dayOfWeek === 4 || dayOfWeek === 7) && isHeHere || isHeHereSpecial)
+	if((dayOfWeek === 4 || dayOfWeek === 7) && isHeHere && hours < 17 || isHeHereSpecial && hours < 17)
 	{
 
 		messageElement.innerHTML = 'He&rsquo;s here!';
 		document.body.classList.add('he-is-here');
 
-	// If it's Wednesday or Saturday and he's not here, show no :(
-	} else if((dayOfWeek === 4 || dayOfWeek === 7) && !isHeHere)
+	// If he was here but market has ended, slightly change the message
+
+	} else if ((dayOfWeek === 4 || dayOfWeek === 7) && isHeHere && hours >= 17 || isHeHereSpecial && hours >= 17)
 	{
 
-		messageElement.innerHTML= 'He&rsquo;s not here :(';
+		messageElement.innerHTML = 'He was here today!';
+
+	// If it's Wednesday or Saturday but he's not here
+
+	} else if ((dayOfWeek === 4 || dayOfWeek === 7) && !isHeHere) {
+
+		messageElement.innerHTML = 'He&rsquo;s not here :(';
 
 	// For all other days it's not market day so he won't be here
+
 	} else {
 
 		messageElement.innerHTML = 'It&rsquo;s not market day, jefferson';
