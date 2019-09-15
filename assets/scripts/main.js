5183a9621e2ce1c8333fddca40b67f52e61ace4d
James Bateson <james@fatmedia.co.uk>
2019-09-15 20:05:37 +0100

    General updates
    - Added wholemeal wrap info
    - Cached custom cursor click state image to avoid flash when first
    clicked
    - Set click position on custom cursor to make it easier (hopefully) to
    click the credit link

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -65,3 +65,25 @@ document.body.addEventListener('mouseup', () => {
 	document.getElementsByTagName('html')[0].style.cursor = 'url(assets/images/clippers.png), auto';
 
 }, false);
+
+function preloadImages(array) {
+	if (!preloadImages.list) {
+		preloadImages.list = [];
+	}
+	const {list} = preloadImages;
+	for (let i = 0; i < array.length; i++) {
+		const img = new Image();
+		img.onload = function () {
+			const index = list.indexOf(this);
+			if (index !== -1) {
+				// remove image from the array once it's loaded
+				// for memory consumption reasons
+				list.splice(index, 1);
+			}
+		};
+		list.push(img);
+		img.src = array[i];
+	}
+}
+
+preloadImages(["assets/images/clippers.png"]);
